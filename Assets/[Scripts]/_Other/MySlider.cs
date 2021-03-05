d5e7abf26208e372a8b3edf1351e7ab7680b6e47
Aykut Alp Turkay <aykutalpturkay@gmail.com>
2021-03-05 22:47:29 +0300

    add progress bar

---
+++ Assets/[Scripts]/_Other/MySlider.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MySlider : MonoBehaviour
+{
+    public Slider slider;
+    public float currentVal;
+    
+    private void Start()
+    {
+        SetMaxVal(100);
+    }
+
+    public void SetVal(float val)
+    {
+        currentVal = val;
+        currentVal = Mathf.Clamp(currentVal, 0, 100);
+        slider.DOValue(val, 0.2f);
+    }
+    private void SetMaxVal(int val)
+    {
+        slider.maxValue = val;
+    }
+    public float GetCurrentVal()
+    {
+        return slider.value;
+    }
+    [Button("IncreaseBar")]
+    public void IncreaseBar(float val)
+    {
+        SetVal(currentVal+val);
+    }
+    [Button("DecreaseBar")]
+    public void DecreaseBar(float val)
+    {
+        SetVal(currentVal-val);
+    }
+    
+    
+    
+    
+}
